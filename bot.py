c6e4784803ec82767c40a2556a92a7dee480b034
Donald <vesper.port@gmail.com>
2023-03-28 14:00:53 +0800

    Test Auto Deploy

--- bot.py
+++ bot.py
@@ -5,7 +5,13 @@ from flask import Flask, request
 from werkzeug.wrappers import Response
 
 from telegram import Bot, Update
-from telegram.ext import Dispatcher, Filters, MessageHandler, CallbackContext
+from telegram.ext import (
+    Dispatcher,
+    Filters,
+    MessageHandler,
+    CallbackContext,
+    CommandHandler,
+)
 
 app = Flask(__name__)
 
@@ -13,14 +19,20 @@ app = Flask(__name__)
 def echo(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(update.message.text)
 
+
+def help_command(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text("Helping you helping you.")
+
+
 bot = Bot(token=os.environ["TOKEN"])
 
 dispatcher = Dispatcher(bot=bot, update_queue=None)
 dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+dispatcher.add_handler(CommandHandler("help", help_command))
+
 
 @app.post("/")
 def index() -> Response:
-    dispatcher.process_update(
-        Update.de_json(request.get_json(force=True), bot))
+    dispatcher.process_update(Update.de_json(request.get_json(force=True), bot))
 
     return "", http.HTTPStatus.NO_CONTENT
