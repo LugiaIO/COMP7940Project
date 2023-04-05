521942d776da0413285039a48f209c27010fb42e
Donald <vesper.port@gmail.com>
2023-04-05 13:08:58 +0800

    fix the bug

--- bot.py
+++ bot.py
@@ -9,7 +9,6 @@ from telegram.ext import (
     Dispatcher,
     Filters,
     MessageHandler,
-    ContextTypes,
     CallbackContext,
     CommandHandler,
     ConversationHandler,
@@ -96,26 +95,26 @@ def imdbTop3Command(update: Update, context: CallbackContext) -> None:
     else:
         update.message.reply_text("No results found!")
 
-def start_note(update: Update, context: ContextTypes.DEFAULT_TYPE):
+def start_note(update: Update, context: CallbackContext) -> None:
     context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! Please input the movie name:")
     return MOVIE_NAME
 
 
-def receive_name(update: Update, context: ContextTypes.DEFAULT_TYPE):
+def receive_name(update: Update, context: CallbackContext) -> None:
     name = update.message.text
     context.user_data['name'] = name
     context.bot.send_message(chat_id=update.effective_chat.id, text="Thanks! Now please input the genre:")
     return MOVIE_GENRE
 
 
-def receive_genre(update: Update, context: ContextTypes.DEFAULT_TYPE):
+def receive_genre(update: Update, context: CallbackContext) -> None:
     genre = update.message.text
     context.user_data['genre'] = genre
     context.bot.send_message(chat_id=update.effective_chat.id, text="Thanks! Now please input the note:")
     return MOVIE_NOTE
 
 
-def receive_note(update: Update, context: ContextTypes.DEFAULT_TYPE):
+def receive_note(update: Update, context: CallbackContext) -> None:
     note = update.message.text
     context.user_data['note'] = note
     context.user_data['username'] = update.effective_user.username
@@ -123,7 +122,7 @@ def receive_note(update: Update, context: ContextTypes.DEFAULT_TYPE):
     addToNote(context.user_data)
     return ConversationHandler.END
 
-def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
+def cancel(update: Update, context: CallbackContext) -> None:
     context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, something went wrong. Conversation canceled.")
     return ConversationHandler.END
 
@@ -144,7 +143,7 @@ dispatcher.add_handler(CommandHandler("random_movie", randomMovieCommand))
 dispatcher.add_handler(CommandHandler("search", searchCommand))
 dispatcher.add_handler(CommandHandler("read_reviews", readReviewsCommand))
 dispatcher.add_handler(CommandHandler("imdb_top_3", imdbTop3Command))
-dispatcher.add_handler(CommandHandler('start_note', start_note))
+dispatcher.add_handler(ConversationHandler('start_note', start_note))
 dispatcher.add_handler(CommandHandler('cancel', cancel))
 
 
